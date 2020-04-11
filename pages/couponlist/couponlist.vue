c68f1b4b7e7b020abc792122e1ec7522b0b19b67
lily <527880475@qq.com>
2020-04-11 14:51:06 +0800

    修改加价券列表界面样式

--- pages/couponlist/couponlist.vue
+++ pages/couponlist/couponlist.vue
@@ -1,6 +1,6 @@
 <template>
 	<view>
-		<view class="swiper-tab viewRow">
+		<view class="swiper-tab row">
 			<view data-current="0" :class="currentTab==0?'selectOn':''" @click="swichNav">待使用</view>
 			<view data-current="1" :class="currentTab==1?'selectOn':''" @click="swichNav">已使用</view>
 			<view data-current="2" :class="currentTab==2?'selectOn':''" @click="swichNav">已失效</view>
@@ -11,125 +11,143 @@
 			<swiper-item class="p15">
 				<scroll-view scroll-y="true" :style="'height:'+scrollHeight+'px;'" data-state="0" @scrolltoupper="refresh"
 				 @scrolltolower="loadMore" upper-threshold="3" lower-threshold="5">
-					<!-- <block :key="index" v-for="(item,index) in list[0]"> -->
+					<block :key="index" v-for="(item,index) in list[1]">
 						<view class="column mb15 coupon">
-							<view class="viewRow" >
-								<view class="couponLeft viewCol txtcenter p10 jccenter">
-									<text class="f20 bold white">10%</text>
+							<view class="row" >
+								<view class="couponLeft column txtcenter p10 jccenter">
+									<text v-if="item.markupCoupon.usedType=='1'" class="f20 bold white">￥{{ item.markupCoupon.usedAmount }}</text>
+									<text v-else-if="item.markupCoupon.usedType=='2'" class="f20 bold white">{{ item.markupCoupon.usedDiscount*100 }}%</text>
 								</view>
 								<view class="couponRight column f14">
 									<view class="row jcbetween aibaseline">
-										<view class="column m10" >
-											<text class="f16 mb10 black bold">订单加价10%</text>
-											<text class="gray-6">2020.3.25 - 2020.4.1</text>
+										<view class="column m10" style="width: 100%;">
+											<view class="row jcbetween">
+												<text class="f16 mb10 black bold mr5">{{ item.markupCoupon.title }}</text>
+												<navigator :url="'../couponorder/couponorder?id='+item.mcId" class="box boxorange white radius4 txtcenter asstart" style="min-width: 60px;">
+													立即使用
+												</navigator>
+											</view>
+											<block v-if="item.markupCoupon.validType==1">
+												<text v-if="item.markupCoupon.validStartTime==item.markupCoupon.validEndTime" class="gray-9 f12">{{ item.markupCoupon.validStartTime }} 当天有效</text>
+												<text v-else class="gray-9 f12">{{ item.markupCoupon.validStartTime }} - {{ item.markupCoupon.validEndTime }}</text>
+											</block>
+											<block v-else-if="item.markupCoupon.validType==2">
+												<text v-if="item.receiveTime==item.receiveTime" class="gray-9 f12 f12">{{ item.receiveTime }} 当天有效</text>
+												<text v-else class="gray-9 f12">{{ item.receiveTime }} - {{ item.receiveTime }}</text>
+											</block>
 										</view>
-										<navigator :url="'../couponorder/couponorder?id=123456'" class="box boxorange white radius4">
-											立即使用
-										</navigator>
 									</view>
-									<view style="border: 1px dashed #EEEEEE; margin-left: 10px;" />
-									<view class="gray-6 row ml5 p5"  @click="showRole">
+									<view style="border: 1px dashed #EEEEEE; margin-left: 10px; margin-right: 10px;" />
+									<view class="gray-6 row ml5 p5"  @click="showRole(item)">
 										使用规则
-										<view class="rout icon-bottom ml5 asend roleIcon" :class="{'roleIcon--active':isOpen}" />
+										<view class="rout icon-bottom ml5 asend roleIcon" :class="{'roleIcon--active':item.isOpen}" />
 									</view>
 								</view>
 							</view>
 							<!-- 加价券使用规则 -->
-							<view class="roleInfo roleInfo--animation" :class="{'roleInfo--hide':!isOpen, 'p15':isOpen}"  :style="{'transform':isOpen?'translateY(0)':'translateY(-10%)','-webkit-transform':isOpen?'translateY(0)':'translateY(-10%)'}">
-								1.使用范围：加价券仅限量心回收内使用。<br />
-								2.订单类型为智能回收订单即可使用。<br />
-								3.使用品类范围：废书纸、废纸皮、易拉罐、杂塑料。<br />
-								4.量心回收保留法律范围内允许的活动解释权。
-							</view>
+							<text class="roleInfo roleInfo--animation" :class="{'roleInfo--hide':!item.isOpen, 'p15':item.isOpen}"  :style="{'transform':item.isOpen?'translateY(0)':'translateY(-10%)','-webkit-transform':item.isOpen?'translateY(0)':'translateY(-10%)'}">
+								{{ item.markupCoupon.contents }}
+							</text>
 						</view>
-					<!-- </block> -->
-					<view v-if="btmp0=='noData'" class="noData">
+					</block>
+					<view v-if="btmp1=='noData'" class="noData">
 						<image src="../../static/images/wujiajiaquan.png" mode="widthFix" style="width: 240px;" />
-						<view>暂无优惠券</view>
+						<view>暂无加价券</view>
 					</view>
-					<view class="bottom" v-if="btmp0=='noMore'">已经到底啦</view>
-					<view class="bottom" v-else-if="btmp0=='loadMore'">加载更多...</view>
+					<view class="bottom" v-if="btmp1=='noMore'">—————— 已经到底啦 ——————</view>
+					<view class="bottom" v-else-if="btmp1=='loadMore'">加载更多...</view>
 				</scroll-view>
 			</swiper-item>
 			<!-- 已使用加价券 -->
 			<swiper-item class="pl5 pt15 pb15">
 				<scroll-view scroll-y="true" :style="'height:'+scrollHeight+'px;'" data-state="1" @scrolltoupper="refresh"
 				 @scrolltolower="loadMore" upper-threshold="3" lower-threshold="5">
-					<!-- <block :key="index" v-for="(item,index) in list[1]"> -->
-					
-					<!-- </block> -->
-					<view v-if="btmp1=='noData'" class="noData">
+					<block :key="index" v-for="(item,index) in list[2]">
+						<view class="column mb15 coupon">
+							<view class="row" >
+								<view class="couponLeft2 column txtcenter p10 jccenter">
+									<text v-if="item.markupCoupon.usedType=='1'" class="f20 bold white">￥{{ item.markupCoupon.usedAmount }}</text>
+									<text v-else-if="item.markupCoupon.usedType=='2'" class="f20 bold white">{{ item.markupCoupon.usedDiscount*100 }}%</text>
+								</view>
+								<view class="couponRight column f14">
+									<view class="row jcbetween aibaseline">
+										<view class="column m10" style="width: 100%;">
+											<text class="f16 mb10 black bold mr5">{{ item.markupCoupon.title }}</text>
+											<block v-if="item.markupCoupon.validType==1">
+												<text v-if="item.markupCoupon.validStartTime==item.markupCoupon.validEndTime" class="gray-9 f12">{{ item.markupCoupon.validStartTime }} 当天有效</text>
+												<text v-else class="gray-9 f12">{{ item.markupCoupon.validStartTime }} - {{ item.markupCoupon.validEndTime }}</text>
+											</block>
+											<block v-else-if="item.markupCoupon.validType==2">
+												<text v-if="item.receiveTime==item.receiveTime" class="gray-9 f12 f12">{{ item.receiveTime }} 当天有效</text>
+												<text v-else class="gray-9 f12">{{ item.receiveTime }} - {{ item.receiveTime }}</text>
+											</block>
+										</view>
+									</view>
+									<view style="border: 1px dashed #EEEEEE; margin-left: 10px; margin-right: 10px;" />
+									<view class="gray-6 row ml5 p5"  @click="showRole(item)">
+										使用规则
+										<view class="rout icon-bottom ml5 asend roleIcon" :class="{'roleIcon--active':item.isOpen}" />
+									</view>
+								</view>
+							</view>
+							<!-- 加价券使用规则 -->
+							<text class="roleInfo roleInfo--animation" :class="{'roleInfo--hide':!item.isOpen, 'p15':item.isOpen}"  :style="{'transform':item.isOpen?'translateY(0)':'translateY(-10%)','-webkit-transform':item.isOpen?'translateY(0)':'translateY(-10%)'}">
+								{{ item.markupCoupon.contents }}
+							</text>
+						</view>
+					</block>
+					<view v-if="btmp2=='noData'" class="noData">
 						<image src="../../static/images/wujiajiaquan.png" mode="widthFix" style="width: 240px;" />
-						<view>暂无优惠券</view>
+						<view>暂无加价券</view>
 					</view>
-					<view class="bottom" v-if="btmp1=='noMore'">已经到底啦</view>
-					<view class="bottom" v-else-if="btmp1=='loadMore'">加载更多...</view>
+					<view class="bottom" v-if="btmp2=='noMore'">—————— 已经到底啦 ——————</view>
+					<view class="bottom" v-else-if="btmp2=='loadMore'">加载更多...</view>
 				</scroll-view>
 			</swiper-item>
 			<!-- 已失效加价券 -->
 			<swiper-item class="pl5 pt15 pb15">
 				<scroll-view scroll-y="true" :style="'height:'+scrollHeight+'px;'" data-state="2" @scrolltoupper="refresh"
 				 @scrolltolower="loadMore" upper-threshold="3" lower-threshold="5">
-					<!-- <block :key="index" v-for="(item,index) in list[1]"> -->
+					<block :key="index" v-for="(item,index) in list[3]">
 						<view class="column mb15 coupon">
-							<view class="viewRow" >
-								<view class="couponLeft2 viewCol txtcenter p10 jccenter">
-									<text class="f20 bold white">10%</text>
+							<view class="row" >
+								<view class="couponLeft2 column txtcenter p10 jccenter">
+									<text v-if="item.markupCoupon.usedType=='1'" class="f20 bold white">￥{{ item.markupCoupon.usedAmount }}</text>
+									<text v-else-if="item.markupCoupon.usedType=='2'" class="f20 bold white">{{ item.markupCoupon.usedDiscount*100 }}%</text>
 								</view>
 								<view class="couponRight column f14">
-									<view class="column m10" >
-										<text class="f16 mb10 black bold">订单加价10%</text>
-										<text class="gray-6">2020.3.25 - 2020.4.1</text>
+									<view class="row jcbetween aibaseline">
+										<view class="column m10" style="width: 100%;">
+											<text class="f16 mb10 black bold mr5">{{ item.markupCoupon.title }}</text>
+											<block v-if="item.markupCoupon.validType==1">
+												<text v-if="item.markupCoupon.validStartTime==item.markupCoupon.validEndTime" class="gray-9 f12">{{ item.markupCoupon.validStartTime }} 当天有效</text>
+												<text v-else class="gray-9 f12">{{ item.markupCoupon.validStartTime }} - {{ item.markupCoupon.validEndTime }}</text>
+											</block>
+											<block v-else-if="item.markupCoupon.validType==2">
+												<text v-if="item.receiveTime==item.receiveTime" class="gray-9 f12 f12">{{ item.receiveTime }} 当天有效</text>
+												<text v-else class="gray-9 f12">{{ item.receiveTime }} - {{ item.receiveTime }}</text>
+											</block>
+										</view>
 									</view>
-									<view style="border: 1px dashed #EEEEEE; margin-left: 10px;" />
-									<view class="gray-6 row ml5 p5"  @click="showRole">
+									<view style="border: 1px dashed #EEEEEE; margin-left: 10px; margin-right: 10px;" />
+									<view class="gray-6 row ml5 p5"  @click="showRole(item)">
 										使用规则
-										<view class="rout icon-bottom ml5 asend roleIcon" :class="{'roleIcon--active':isOpen}" />
+										<view class="rout icon-bottom ml5 asend roleIcon" :class="{'roleIcon--active':item.isOpen}" />
 									</view>
 								</view>
 							</view>
 							<!-- 加价券使用规则 -->
-							<view class="roleInfo roleInfo--animation" :class="{'roleInfo--hide':!isOpen, 'p15':isOpen}"  :style="{'transform':isOpen?'translateY(0)':'translateY(-10%)','-webkit-transform':isOpen?'translateY(0)':'translateY(-10%)'}">
-								1.使用范围：加价券仅限量心回收内使用。<br />
-								2.订单类型为智能回收订单即可使用。<br />
-								3.使用品类范围：废书纸、废纸皮、易拉罐、杂塑料。<br />
-								4.量心回收保留法律范围内允许的活动解释权。
-							</view>
+							<text class="roleInfo roleInfo--animation" :class="{'roleInfo--hide':!item.isOpen, 'p15':item.isOpen}"  :style="{'transform':item.isOpen?'translateY(0)':'translateY(-10%)','-webkit-transform':item.isOpen?'translateY(0)':'translateY(-10%)'}">
+								{{ item.markupCoupon.contents }}
+							</text>
 						</view>
-					
-					
-						<view class="column mb15 coupon">
-							<view class="viewRow" >
-								<view class="couponLeft2 viewCol txtcenter p10 jccenter">
-									<text class="f20 bold white">￥5</text>
-								</view>
-								<view class="couponRight column f14">
-									<view class="column m10" >
-										<text class="f16 mb10 black bold">订单加价5元</text>
-										<text class="gray-6">2020.3.25 - 2020.4.1</text>
-									</view>
-									<view style="border: 1px dashed #EEEEEE; margin-left: 10px;" />
-									<view class="gray-6 row ml5 p5"  @click="showRole">
-										使用规则
-										<view class="rout icon-bottom ml5 asend roleIcon" :class="{'roleIcon--active':isOpen}" />
-									</view>
-								</view>
-							</view>
-							<!-- 加价券使用规则 -->
-							<view class="roleInfo roleInfo--animation" :class="{'roleInfo--hide':!isOpen, 'p15':isOpen}"  :style="{'transform':isOpen?'translateY(0)':'translateY(-10%)','-webkit-transform':isOpen?'translateY(0)':'translateY(-10%)'}">
-								1.使用范围：加价券仅限量心回收内使用。<br />
-								2.订单类型为智能回收订单即可使用。<br />
-								3.使用品类范围：废书纸、废纸皮、易拉罐、杂塑料。<br />
-								4.量心回收保留法律范围内允许的活动解释权。
-							</view>
-						</view>
-					<!-- </block> -->
-					<view v-if="btmp2=='noData'" class="noData">
+					</block>
+					<view v-if="btmp3=='noData'" class="noData">
 						<image src="../../static/images/wujiajiaquan.png" mode="widthFix" style="width: 240px;" />
-						<view>暂无优惠券</view>
+						<view>暂无加价券</view>
 					</view>
-					<view class="bottom" v-if="btmp2=='noMore'">已经到底啦</view>
-					<view class="bottom" v-else-if="btmp2=='loadMore'">加载更多...</view>
+					<view class="bottom" v-if="btmp3=='noMore'">—————— 已经到底啦 ——————</view>
+					<view class="bottom" v-else-if="btmp3=='loadMore'">加载更多...</view>
 				</scroll-view>
 			</swiper-item>
 		</swiper>
@@ -144,12 +162,11 @@
 			return {
 				currentTab: 0, // 当前Tab
 				scrollHeight: 0, // 设备高度
-				btmp0: 'noMore', // 待使用scroll底部panel
-				btmp1: 'noData', // 已使用scroll底部panel
-				btmp2: 'loadMore', // 已失效scroll底部panel
+				btmp1: 'loadMore', // 待使用scroll底部panel
+				btmp2: 'loadMore', // 已使用scroll底部panel
+				btmp3: 'loadMore', // 已失效scroll底部panel
 				list: [], // id:1==待使用  id:2==已使用		id:3==已失效
 				page: [] ,// 1:待使用  2:已使用		3:已失效	
-				isOpen: false
 			}
 		},
 		methods: {
@@ -158,21 +175,118 @@
 				let that = this;
 				let curr = e.currentTarget.dataset.current;
 				that.currentTab = curr;
-					// that.getList(1, curr, 'new');
+				that.getList(1, curr, 'new');
 			},
 			// 滑动swiper获取数据
 			bindChange: function(e) {
 				let that = this;
 				let curr = e.detail.current;
 				that.currentTab = curr;
-				// that.getList(1, curr, 'new');
+				that.getList(1, curr, 'new');
 			},
-			// 使用规则
-			showRole() {
-				console.log(this)
-				this.isOpen = !this.isOpen;
+			// 点击打开使用规则
+			showRole(item) {
+				item.isOpen = !item.isOpen;
 				this.$forceUpdate();//强制重新渲染
-			}
+			},
+			// 下拉获取最新数据
+			refresh: function(e) {
+				let that = this;
+				clearTimeout(that.timer);
+				if (that.isLoading == false) {
+					that.isLoading = true;
+					setTimeout(function() {
+						uni.startPullDownRefresh({
+							success(res) {
+								console.log("refresh开始下拉"); 
+							}
+						})
+					}, 350);
+				}
+			},
+			// 上拉加载更多
+			loadMore: function(e) {
+				let that = this;
+				if (that.isLoading == true) { // 避免多次触发获取更多function
+					that.$util.showToast("正在获取数据", 'none', 2000);
+					return;
+				}
+				let tab = that.currentTab; // 获取当前显示Tab
+				let state = 0;
+				if (tab == "0") { // 1号swiper-item加载待使用加价券数据
+					state = 1;
+				} else if (tab == "1") { // 2号swiper-item加载已使用加价券数据
+					state = 2;
+				} else if (tab == "2") { // 3号swiper-item加载已失效加价券数据
+					state = 3;
+				}
+				let btmp = "btmp" + state;
+				let p = that.page[state]; // 获取state对应分页数据
+				if (p.currentPage < p.allPageAmount) { // 当前页小于总页数,底部panel显示加载中,执行获取加价券function
+					that[`${btmp}`] = "loadMore";
+					that.getList(p.currentPage + 1, tab, "loadMore");
+				}
+			},
+			//获取加价券列表
+			getList: function(curr, tab, rid) {
+				let that = this; 
+				let state = 0;
+				if (tab == "0") { // 1号swiper-item加载待使用加价券数据
+					state = 1;
+				} else if (tab == "1") { // 2号swiper-item加载已使用加价券数据
+					state = 2;
+				} else if (tab == "2") { // 3号swiper-item加载已失效加价券数据
+					state = 3;
+				}
+				let data = {
+					pageSize: 6,
+					currPage: curr,
+					state: state
+				};
+				uni.showNavigationBarLoading(); // 标题栏显示加载状态
+				that.$showLoading(); // 显示遮罩
+				that.isLoading = true; // 更改读取状态
+				let btmp = "btmp" + state; // list底部状态
+				that.$request.postToken("/users/marcourecord/findPage.do", data).then((res) => {
+					if (res.data.status === 0) {
+						let btm = (res.data.page.currentPage >= res.data.page.allPageAmount) ? 'noMore' : 'loadMore';
+						data = JSON.parse(res.data.results);
+						console.log(data)
+						data.forEach(item => {
+							item.isOpen = false;
+							item.markupCoupon.validStartTime = (item.markupCoupon.validStartTime!=null)?item.markupCoupon.validStartTime.slice(0,10):'';//使用开始时间
+							item.markupCoupon.validEndTime = (item.markupCoupon.validEndTime!=null)?item.markupCoupon.validEndTime.slice(0,10):'';//使用结束时间
+							item.receiveTime = (item.receiveTime!=null)?item.receiveTime.slice(0,10):'';	//领取时间
+							item.markupCoupon.delayTime = (item.markupCoupon.delayTime!=null)?item.markupCoupon.delayTime.slice(0,10):'';//相对结束时间
+						})
+						if (rid == "new") { // 判断当前读取是刷新读取还是加载读取
+							that.$set(that.list,state,data);
+						} else {
+							that.$set(that.list,state,that.list[state].concat(data));
+						}
+						that.$set(that.page,state,res.data.page);
+						that[`${btmp}`] = btm;
+					} else {
+						that.$util.showToast(data, 'none', 5000);
+					}
+				}).catch((err) => {
+					console.log(err);
+					that.$util.showToast(err.errMsg, 'none', 5000);
+					if (err.errMsg === '401') {
+						uni.redirectTo({
+							url: '../login/login'
+						});
+					}
+				}).finally(() => {
+					if (that.list[state] == null || that.list[state].length == 0) {
+						that[`${btmp}`] = "noData"
+					}
+					that.$hideLoading();
+					uni.hideNavigationBarLoading();
+					uni.stopPullDownRefresh();
+					that.isLoading = false; // 更改读取状态
+				})
+			},
 		},
 		onLoad(options) {
 			let that = this;
@@ -180,13 +294,12 @@
 			if (options.state != null) {
 				that.currentTab = options.state;
 			}
-			// that.getList(1, that.currentTab, 'new');
+			that.getList(1, that.currentTab, 'new');
 		},
 		//监听用户下拉刷新事件
 		onPullDownRefresh() {
 			let that = this;
-			let state = that.currentTab;
-			// that.getList(1, state, 'new');
+			that.getList(1, that.currentTab, 'new');
 		}
 	}
 </script> 
@@ -237,17 +350,16 @@
 		background: #999999;
 		overflow: hidden;
 	}
-	.couponLeft::after, .couponLeft2::after {
+	/* .couponLeft::after, .couponLeft2::after {
 		content: '';
 		position: absolute;
 		top: 0px;
 		bottom: 0px;
 		left: -6px;
 		width: 12px;
-		height: 100%;
 		background: radial-gradient(circle, #eeeeee, #eeeeee 4px, transparent 4px);
 		background-size: 12px 12px;
-	}
+	} */
 	.couponRight {
 		background: #FFFFFF;
 		width: 260px;
