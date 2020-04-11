32502f441493c040a94ca667074c369045df3e75
lily <527880475@qq.com>
2020-04-11 15:11:01 +0800

    界面样式修改，订单列表只显示单选按钮，品种名，订单总价和奖励金

--- pages/couponorder/couponorder.vue
+++ pages/couponorder/couponorder.vue
@@ -1,7 +1,7 @@
 <template>
 	<view>
 		<!-- 加价券详情 -->
-		<view class="couponInfo bg-white radius4 m10 p10 mb20">
+		<view class="couponInfo bg-white mb20 p10">
 			<view class="bold f16 blue1">订单加价10%</view>
 			<view class="gray-9">
 				可选择订单的时间：2020.04.01 - 2020.04.07</text>
@@ -10,14 +10,13 @@
 		<!-- 可使用的订单列表 -->
 		<view class="orderList bg-white">
 			<text class="f16">可选择的订单：</text>
-			<scroll-view scroll-y="true" style="height: 300px;" class="mt10">
+			<scroll-view scroll-y="true" style="height: 300px;" class="mt10 mb5">
 				<radio-group @change="radioChange" class="column f14">
 					<label class="row aicenter pb5" v-for="(item, index) in list" :key="item.value">
 						<radio :value="item.value" :checked="index === current" style="transform: scale(0.7)" />
-						<text class="" style="width: 25%;">{{item.checkTime}}</text>
-						<text class="" style="width: 25%;">{{item.actType}}</text>
-						<text class="" style="width: 20%;">{{item.actAmount}}公斤</text>
-						<text class="red" style="width: 20%;">￥{{item.actTotalPrice}}</text>
+						<text class="" style="width: 45%;">{{item.actType}}</text>
+						<text class="gray-9" style="width: 20%;">￥{{item.actTotalPrice}}</text>
+						<text class="red" style="width: 20%;">+￥{{ item.rewardPrice }}</text>
 					</label>
 				</radio-group>
 			</scroll-view>
@@ -31,111 +30,138 @@
 	export default {
 		data() {
 			return {
-				current: 0,
+				current: '',		//选中的订单
 				list: [{
-					value: '订单编号',
+					value: '订单编号1',
 					checkTime: '2020-04-01',
 					actType: '废纸皮',
-					actAmount: '0.2',
-					actTotalPrice: '0.4'
+					actTotalPrice: '0.4',
+					rewardPrice: '0.04',	//使用加价券后的返利
 				},{
-					value: '订单编号',
+					value: '订单编号2',
 					checkTime: '2020-04-01',
 					actType: '废书纸',
-					actAmount: '0.4',
-					actTotalPrice: '0.8'
+					actTotalPrice: '0.8',
+					rewardPrice: '0.08',	//使用加价券后的返利
 				},{
-					value: '订单编号',
+					value: '订单编号3',
 					checkTime: '2020-04-02',
 					actType: '中铝易拉罐',
-					actAmount: '0.2',
-					actTotalPrice: '0.4'
+					actTotalPrice: '0.4',
+					rewardPrice: '0.04',	//使用加价券后的返利
 				},{
-					value: '订单编号',
+					value: '订单编号4',
 					checkTime: '2020-04-03',
 					actType: '全新书籍',
-					actAmount: '0.2',
-					actTotalPrice: '0.4'
+					actTotalPrice: '2.4',
+					rewardPrice: '0.24',	//使用加价券后的返利
 				},{
-					value: '订单编号',
+					value: '订单编号5',
 					checkTime: '2020-04-04',
 					actType: '全新纸皮箱',
-					actAmount: '0.2',
-					actTotalPrice: '0.4'
+					actTotalPrice: '11.4',
+					rewardPrice: '1.14',	//使用加价券后的返利
 				},{
-					value: '订单编号',
+					value: '订单编号6',
 					checkTime: '2020-04-05',
 					actType: '优质杂塑料',
-					actAmount: '0.2',
-					actTotalPrice: '0.4'
+					actTotalPrice: '17',
+					rewardPrice: '0.17',	//使用加价券后的返利
 				},{
-					value: '订单编号',
+					value: '订单编号7',
 					checkTime: '2020-04-06',
 					actType: '残污衣物',
-					actAmount: '0.44',
-					actTotalPrice: '0.66'
+					actTotalPrice: '0.66',
+					rewardPrice: '0.06',	//使用加价券后的返利
 				},{
-					value: '订单编号',
+					value: '订单编号8',
 					checkTime: '2020-04-07',
 					actType: '费旧报纸',
-					actAmount: '0.12',
-					actTotalPrice: '0.31'
+					actTotalPrice: '0.31',
+					rewardPrice: '0.03',	//使用加价券后的返利
 				},{
-					value: '订单编号',
+					value: '订单编号9',
 					checkTime: '2020-04-07',
 					actType: '费旧报纸',
-					actAmount: '0.12',
-					actTotalPrice: '0.31'
+					actTotalPrice: '6.31',
+					rewardPrice: '0.63',	//使用加价券后的返利
 				},{
-					value: '订单编号',
+					value: '订单编号10',
 					checkTime: '2020-04-07',
 					actType: '费旧报纸',
-					actAmount: '0.12',
-					actTotalPrice: '0.31'
+					actTotalPrice: '5.31',
+					rewardPrice: '0.53',	//使用加价券后的返利
 				},{
-					value: '订单编号',
+					value: '订单编号11',
 					checkTime: '2020-04-07',
 					actType: '费旧报纸',
-					actAmount: '0.12',
-					actTotalPrice: '0.31'
+					actTotalPrice: '12.0',
+					rewardPrice: '1.2',	//使用加价券后的返利
 				},{
-					value: '订单编号',
+					value: '订单编号12',
 					checkTime: '2020-04-07',
 					actType: '费旧报纸',
-					actAmount: '0.12',
-					actTotalPrice: '0.31'
+					actTotalPrice: '0.31',
+					rewardPrice: '0.03',	//使用加价券后的返利
 				},{
-					value: '订单编号',
+					value: '订单编号13',
 					checkTime: '2020-04-07',
 					actType: '费旧报纸',
-					actAmount: '0.12',
-					actTotalPrice: '0.31'
+					actTotalPrice: '0.31',
+					rewardPrice: '0.03',	//使用加价券后的返利
 				}
 				]
 			}
 		},
 		methods: {
-			//根据加价券获取可使用的订单列表
-			getOrder(id) {
-				let that = this;
-				console.log(id)
-			},
 			//订单选择
 			radioChange: function(e) {
 				let that = this;
 				for (let i = 0; i < that.list.length; i++) {
 					if (that.list[i].value === e.target.value) {
-						this.current = i;
+						that.current = i; 
 						break;
 					}
 				}
-				console.log(e)
+			},
+			//获取加价券详情以及可使用的订单列表
+			getList(id) {
+				let that = this;
+				let mcrId = id;
+				that.$request.postToken("/users/marcourecord/findPage.do", mcrId).then((res) => {
+					if (res.data.status === 0) {
+						console.log(res)
+						// res = JSON.parse(res.data.results);
+					} else {
+						that.$util.showToast(data, 'none', 5000);
+					}
+				}).catch((err) => {
+				console.log(err);
+				that.$util.showToast(err.errMsg, 'none', 5000);
+				if (err.errMsg === '401') {
+					uni.redirectTo({
+						url: '../login/login'
+					});
+				}
+				}).finally(() => {
+					that.$hideLoading();
+				})
+			},
+			//立即使用
+			sendOrder() {
+				let that = this;
+				that.$util.showToast('加价成功，即将返回订单列表。', 'none', 2500);
+				setTimeout(function() {
+					uni.switchTab({
+						url: '../orderlist/orderlist'
+					})
+				},2500);
 			}
 		},
 		onLoad(options) {
 			let that = this;
 			if (that.$stringUtil.isNotEmpty(options.id)) {
-				that.getOrder(options.id);
+				that.getList(options.id);//获取加价券详情以及可使用的订单列表
 			}
 		}
 	}
@@ -143,7 +169,7 @@
 
 <style>
 	.couponInfo {
-		box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
+		/* box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04); */
 	}
 	.orderList {
 		padding: 10px 15px;
