d83b412afaac752bf98c7a4115d5806475daebcc
lily <527880475@qq.com>
2020-04-18 10:49:49 +0800

    实名标签样式修改

--- pages/personal/personal.vue
+++ pages/personal/personal.vue
@@ -249,6 +249,7 @@
 		border-radius: 25px 0 0 25px;
 		position: relative;
 		right: -3px;
+		top: -10px;
 		overflow: hidden;
 		box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
 	}
