6cf3e82e5f66650c660571e6bb4c651e4bfdbfe7
lily <527880475@qq.com>
2020-04-22 09:14:48 +0800

    新增可回收物页面（包括政府回收、保洁回收、商户回收功能），联系我们页面（客服电话，客服微信二维码

---
+++ pages/recyclable/recyclable.vue
@@ -0,0 +1,202 @@
+<template>
+	<view>
+		<image src="../../static/images/visit_banner.png" mode="widthFix" style="width: 100%;"/>
+		<view class="gray-9 mt15 mb15 txtcenter">————<text class="f16 bold mb10 gray-6 ml5 mr5 "> 选择回收类型 </text>————</view>
+		<view class="column m15 txtcenter">
+			<view class="row mb10 f16 bold">
+				<view class="line" />
+				<text>量心币</text>
+			</view> 
+			<view class="row mb25 ml5">
+				<view class="colum mr25" @click="tapOpenPanel('政府回收')">
+					<image class="img" src="../../static/images/tu1.png" mode="widthFix" />
+					<view class="gray-9">政府回收</view>
+				</view>
+			</view>
+			<view class="row mb10 f16 bold">
+				<view class="line" />
+				<text>零钱</text>
+			</view> 
+			<view class="row mb25 ml5">
+				<view class="colum mr25" @click="tapOpenPanel('商户回收')">
+					<image class="img" src="../../static/images/tu4.png" mode="widthFix" />
+					<view class="gray-9">商户回收</view>
+				</view>
+				<view class="colum mr25" @click="tapOpenPanel('保洁回收')">
+					<image class="img" src="../../static/images/tu6.png" mode="widthFix" />
+					<view class="gray-9">保洁回收</view>
+				</view>
+			</view>
+			
+			<!-- 呼叫回收面板 -->
+			<view v-show="formPanel" class="mask" />
+			<view v-show="formPanel" class="dialog txtcenter">
+				<form @submit="sendForm(title)" report-submit="true">
+					<view class="bg-white radius6 pb5">
+						<view class="row jcend ">
+							<view class=" pt10 f18">{{ title }}</view>
+							<view @click='tapClosePanel' class="rout icon-roundclose gray f26 m5" style="margin-left: 30%;" />
+						</view>
+						<!--收货地址-->
+						<navigator class="row jcleft aicenter m10 address radius6 f14" url="../addresslist/addresslist?parent=government">
+							<view class="rout icon-dizhi txtcenter f20" />
+							<view class="elips">
+								<text v-if="address.contact!=null">{{address.contact}} - {{address.tel}}\n{{address.area}}{{address.address}}</text>
+								<text v-else class="">请选择收货地址</text>
+							</view>
+							<view class="rout icon-dayuhao overhidden txtcenter" />
+						</navigator>
+						<button class="f16 white bg-blue1 radiu6" formType="submit" style="width: 80%;">呼叫回收</button>
+					</view>
+				</form>
+			</view>
+			
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				formPanel: false, // 呼叫回收面板
+				customer: {},
+				address: {},
+				title: "",	//呼叫回收面板标题
+			}
+		},
+		methods: {
+			// 弹出预约回收面板
+			tapOpenPanel: function(title) {
+				let that = this;
+				that.title = title;	//面板标题，选中的回收类型
+				that.formPanel = true;
+			},
+			// 关闭预约回收面板
+			tapClosePanel: function() {
+				let that = this;
+				that.formPanel = false;
+			},
+			// 呼叫回收
+			sendForm: function(title) {
+				let that = this;
+				let address = that.address;
+				if (that.$stringUtil.isEmpty(address.contact)) {
+					that.$util.showToast("请选择收货地址", "none", 4000)
+					return false;
+				}
+				that.$showLoading(); // 显示遮罩
+				let temp = address; // 添加地址
+				if (title == "政府回收") {
+					temp.recyType = '1'; //回收类型，量心币回收
+				}else if (title == "商户回收" || title == "保洁回收") {
+					temp.recyType = '4'; //回收类型，零钱回收
+				}
+				that.$request.postToken("/users/recyorder/government.do", {
+					data: JSON.stringify(temp)
+				}).then((res) => {
+					//console.log(res);
+					that.$util.showToast(res.data.results, 'none', 3000);
+					that.formPanel = false;
+				}).catch((err) => {
+					console.log(err);
+					that.$util.showToast(err, 'none', 5000);
+				}).finally(() => {
+					that.$hideLoading();
+				})
+			},
+			//	获取用户信息
+			getCustomerInfo: function() {
+				let that = this;
+				that.$showLoading()
+				that.$request.postToken("/users/customer/findInfo.do", null).then((res) => {
+					if (res.data.status === 0) {
+						that.customer = JSON.parse(res.data.results);
+					} else {
+						that.$util.showToast(res.data.results, 'none', 5000);
+					}
+				}).catch((err) => {
+					console.log(err);
+					that.$util.showToast(err, 'none', 5000);
+				}).finally(() => {
+					that.$hideLoading();
+				})
+			},
+			//设置用户的默认地址
+			getDefaultAddr: function() {
+				let that = this;
+				that.$showLoading(); // 显示遮罩
+				that.$request.postToken("/users/address/findDefaultAddress.do", null).then((res) => {
+					if (res.data.status === 0) {
+						let address = JSON.parse(res.data.results);
+						uni.setStorageSync('address', address);
+						that.address = address;
+					} else {
+						uni.removeStorageSync('address');
+						that.$util.showToast(res.data.results, 'none', 5000);
+					}
+				}).catch((err) => {
+					console.log(err);
+					that.$util.showToast(err, 'none', 5000);
+				}).finally(() => {
+					that.$hideLoading();
+				})
+			}
+		},
+		onLoad() {
+			let that = this;
+			that.getDefaultAddr(); // 获取默认地址
+		},
+		onShow() {
+			let that = this;
+			let address = uni.getStorageSync("address"); // 读取缓存中地址信息
+			if (address != null) {
+				that.address = address;
+			}
+			this.getCustomerInfo();	//	获取用户基本信息
+		}
+	}
+</script>
+
+<style>
+	.img {
+		width: 48px;
+	}
+	
+	.line {
+		width: 5px;
+		background: #00A2ED;
+		margin-right: 5px;
+	}
+	/* 呼叫回收面板样式 */
+	.dialog {
+		position: fixed;
+		z-index: 101;
+		width: 85%;
+		max-width: 300px;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		-webkit-transform: translate(-50%, -50%);
+	}
+	
+	/* 收货地址样式 */
+	.address {
+		height: 55px;
+		vertical-align: middle;
+		border: 2px dashed #ccc;
+		text-align: left;
+	}
+	
+	.address>view:nth-child(1) {
+		width: 14%;
+	}
+	
+	.address>view:nth-child(2) {
+		width: 76%;
+	}
+	
+	.address>view:nth-child(3) {
+		width: 10%;
+	}
+</style>
---
+++ pages/service/service.vue
@@ -0,0 +1,73 @@
+<template>
+	<view class="m15">
+		<!-- 联系电话 -->
+		<view class="colum p10 bg-white card txtcenter mb25">
+			<view class="f18 orange">服务热线</view>
+			<view class="mt5 mb10">+ 4007705180</view>
+			<view class="telBtn" @click='call'>立即拨打</view>
+		</view>
+		<!-- 客服微信二维码 -->
+		<view class="colum p10 bg-white card txtcenter mb25">
+			<view class="f18 orange">客服微信</view>
+			<view class="gray-9">扫一扫“二维码名片”，即可添加客服微信。</view>
+			<view @longpress="saveImg()">
+				<image class="img" src="../../static/images/kefu.png" mode="widthFix" @click="tapPreviewImage" />
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				host: ''
+			}
+		},
+		methods: {
+			// 联系我们
+			call: function() {
+				var that = this;
+				var phone = "4007705180";
+				uni.makePhoneCall({
+					phoneNumber: phone,
+					fail: function(err) { 
+						console.log(err);
+						that.$util.showToast(err.errMsg, 'none', 3000);
+					}
+				})
+			},
+			// 预览图片
+			tapPreviewImage: function() {
+				let that = this;
+				let imgUrl = [];
+				imgUrl.push(that.host+"/static/images/kefu.png");
+				console.log(imgUrl)
+				uni.previewImage({ // 预览图片
+					urls: imgUrl // 所有要预览的图片 
+				})
+			},
+		},
+		onLoad() {
+			let that = this;
+			that.host = that.$app.globalData.host;
+			// that.host = "http://127.0.0.1:22053/__pageframe__";//测试
+		}
+	}
+</script>
+
+<style>
+.telBtn {
+	width: 70%;
+	border-radius: 10px;
+	background-color: #FF8800;
+	line-height: 2em;
+	color: #FFFFFF;
+	margin: 0 auto;
+	font-size: 18px;
+}
+.img {
+	width: 80%;
+	pointer-events: auto !important;
+}
+</style>
