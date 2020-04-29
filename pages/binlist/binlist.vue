b3700a607782ae9a95ca1b850466e2c0e29421ee
lily <527880475@qq.com>
2020-04-29 17:15:18 +0800

    获取定位type

--- pages/binlist/binlist.vue
+++ pages/binlist/binlist.vue
@@ -177,7 +177,7 @@
 		onReady() {
 			let that = this;
 			uni.getLocation({	//获取附近站点列表
-				type: 'gcj02 ',
+				type: 'gcj02',
 				success: function (res) {
 					console.log('当前位置的经度：' + res.longitude);
 					console.log('当前位置的纬度：' + res.latitude);
